22e02597ab5c37035ff55ee9021d7a34564b87a8
Jacob Mimms <jacobmimms@gmail.com>
2020-12-06 22:04:25 -0500

    Initial commit, Day 1-2

---
+++ 2/Advent2.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+// Your First Program
+
+public class Advent2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("Hello, World!"); 
+        File input = new File("1_input.txt");
+        Scanner scan  = new Scanner(input);
+
+        System.out.println(scan.nextLine());
+        scan.close();
+    }
+}
